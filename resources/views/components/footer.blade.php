97de0a829e214b42db6c96a09283cbf35c0eb855
GonzalezValadez18 <jleonardo18071999@gmail.com>
2025-05-06 15:32:41 -0600

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="bg-gray-800 text-white text-center p-4 mt-8">
+       <!--- Contenido del footer con el año del sistema--->
+    <p>&copy; {{ date('Y') }} FarmaPro. Todos los derechos reservados.</p>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmacia | FarmaPro</title>
+    @vite(['./resources/css/app.css', './resources/js/app.js'])
+</head>
+
+
+<x-header />
+<body>
+    <main class="container mx-auto mt-8 px-4">
+        <p class="text-2xl/8 font-sans font-bold">FarmaPro</p>
+        <x-carrousel />
+    </main>
+    
+</body>
+
+
+<x-footer />
+
+</body>
+</html>
